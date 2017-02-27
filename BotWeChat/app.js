9ad7844b74295a9acd1eb2f78e404ef864203778
ChenWes <chenxuhua0530@163.com>
2017-02-27 16:51:05 +0800

    add wechat function , just can send message to client

--- BotWeChat/app.js
+++ BotWeChat/app.js
@@ -5,16 +5,25 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var winston = require('winston');
+var wechat = require('wechat');
 
 var app = express();
 
-
+//setting logger
 var logger = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)(),
     new (winston.transports.File)({ filename: './log/bot-wechat.log' })
   ]
 });
+
+//wechat config
+var config = {
+  token: 'weixin',
+  appid: 'wxf6d0ac7f84dc22fb',
+  encodingAESKey: 'ZEtViedarf49EUOCDeu45pqhkZhKPFBjSHI2DynP4vq',
+  checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false 
+};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +36,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//this can get message from WeChat server, and can send message to wechat client
+//此处监控的是URL的wechat，那么在配置微信的URL时，也需要在主机URL地址后面加入wechat这样才可以获取到数据
+app.use(express.query());
+app.use('/wechat', wechat(config, function (req, res, next) {
+  //微信输入信息都在req.weixin上 
+  var message = req.weixin;
+  logger.log('info',message);
+
+  res.reply('reply message');
+}));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
