bd253ea55a89d8a7d192ebc435677c7687d9c44c
ChenWes <chenxuhua0530@163.com>
2017-02-27 15:39:07 +0800

    project init , if have error , we need create log folder

---
+++ BotWeChat/app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var winston = require('winston');
+
+var app = express();
+
+
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({ filename: './log/bot-wechat.log' })
+  ]
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var title = 'esquel';
+  var err = new Error(':) Page Not Found');
+  err.status = 404;
+
+  logger.log('error', "Req.url:" + req.url + "    :) Page Not Found");
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
